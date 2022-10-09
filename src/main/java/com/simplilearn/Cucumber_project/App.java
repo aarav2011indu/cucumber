8e57ab83493e9026581e25b506722109f4117eff
indu sharma <indusharma.er@gmail.com>
2022-10-09 12:34:13 +0530

    first commit

---
+++ src/main/java/com/simplilearn/Cucumber_project/App.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Cucumber_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
